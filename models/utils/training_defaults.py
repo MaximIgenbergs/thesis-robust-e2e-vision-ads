054f59ef9dcaafbbdb005900b2c9e92482922bd0
Maxim Igenbergs <migenbergs@gmail.com>
2025-06-12 17:06:59 +0200

    new settings

--- models/utils/training_defaults.py
+++ models/utils/training_defaults.py
@@ -1,8 +1,8 @@
 # Default hyperparameters for training
 BATCH_SIZE = 64
-EPOCHS = 100
-PATIENCE = 5
+EPOCHS = 120
+PATIENCE = 10
 LEARNING_RATE = 0.0001
-ALPHA_STEER = 0.8 # steering weight in combined loss
+ALPHA_STEER = 0.7 # steering weight in combined loss
 VAL_SPLIT = 0.2
 RANDOM_SEED = 42
